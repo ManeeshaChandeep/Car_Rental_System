0cb654db254e915d3db27c44a5b9c92d97e912f7
ManeeshaChandeep <maneeshachandeep@gmail.com>
2023-10-31 22:40:55 +0530

    update

--- Back-End/src/main/java/spring/WebAppInitializer.java
+++ Back-End/src/main/java/spring/WebAppInitializer.java
@@ -1,4 +1,22 @@
 package spring;
 
-public class WebAppInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import spring.config.WebAppConfig;
+import spring.config.WebRootConfig;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
