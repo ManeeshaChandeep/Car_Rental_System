966978616443fd978fe55a9232e832c94750c58b
ManeeshaChandeep <maneeshachandeep@gmail.com>
2023-11-02 10:13:13 +0530

    update

---
+++ Front-End/controllers/driverLoginController.js
@@ -0,0 +1,18 @@
+function getDriverOB(){
+    let driver={
+
+       nicnumber:$('#txtNICNumber').val(),
+       licensecardnumber:$('#txtLicenseCardNumber').val(),
+       yourname:$('#txtYourName').val(),
+       address:$('#txtAddress').val(),
+       availability:$('#txtAvailability').val(),
+       phonenumber:$('#txtPhoneNumber').val(),
+       email:$('#txtEmail').val(),
+       username:$('#txtUserName').val(),
+       password:$('#txtPassword').val(),
+
+
+    }
+
+
+}
